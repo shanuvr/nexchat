52f0b3293d6a3b7c5a4739cc86a0c2dbe005920d
Shanu V Ramesan <shanuvr112233@gmail.com>
2026-03-19 17:39:35 +0530

    feat: implimented rate limiting

--- api-gateway/src/middleware.ts/rateLimiter.ts
+++ api-gateway/src/middleware.ts/rateLimiter.ts
@@ -1,12 +1,12 @@
 import rateLimit from "express-rate-limit";
 export const generalRateLimiter = rateLimit({
-    windowMs:15 * 60 * 1000,
-    max:100,
-    message:{error:"too many request please try again"}
-})
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: { error: "too many request please try again" },
+});
 
 export const authLimiter = rateLimit({
-    windowMs:15 * 60 * 1000,
-    max:5,
-    message:{error : "too many login attempts please  try again later"}
-})
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: { error: "too many login attempts please  try again later" },
+});
